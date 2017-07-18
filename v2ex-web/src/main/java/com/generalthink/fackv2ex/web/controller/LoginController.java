be0e14b1097a7f48aa6d9c37c2c135a9ef3fbc95
TKing <general_go@163.com>
2017-07-18 22:19:47 +0800

    添加POM dependency支持，添加基本页面，使用beetl管理，保证首页可以访问

---
+++ v2ex-web/src/main/java/com/generalthink/fackv2ex/web/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.generalthink.fackv2ex.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/index")
+    public String index() {
+        
+        return "index";
+    }
+
+}
